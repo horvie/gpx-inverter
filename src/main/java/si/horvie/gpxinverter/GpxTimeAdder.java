0650dc3be2ba54bbcaa2a6049d08f50d2134b529
David Horvat <david.horvat@artes.si>
2020-03-09 08:23:24 +0100

    Add time adder

--- src/main/java/si/horvie/gpxinverter/GpxTimeAdder.java
+++ src/main/java/si/horvie/gpxinverter/GpxTimeAdder.java
@@ -2,31 +2,33 @@ package si.horvie.gpxinverter;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
-public class Main {
+public class GpxTimeAdder {
   private static final String CONTENT_PATH = "src/main/resources";
+  private static final DateTimeFormatter DF =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
   public static void main(String[] args) throws Exception {
-    String filename = "original.gpx";
+    String filename = "time.gpx";
 
     Document doc =
         DocumentHelper.parseText(
             new String(Files.readAllBytes(Paths.get(CONTENT_PATH + "/" + filename))));
 
     Element trkseg = doc.getRootElement().element("trk").element("trkseg");
-    List<Element> newEls = new ArrayList<>();
-    List<Element> els = trkseg.elements();
-    els.forEach(el -> newEls.add((Element) el.detach()));
-
-    Collections.reverse(newEls);
-    newEls.forEach(el -> trkseg.add(el));
+    List<Element> trkpts = trkseg.elements();
+    LocalDateTime time = LocalDateTime.of(2016, 8, 15, 12, 0);
+    for (Element trkpt : trkpts) {
+      trkpt.addElement("time").setText(DF.format(time));
+      time = time.plusMinutes(1);
+    }
 
     System.out.println(doc.asXML());
   }
